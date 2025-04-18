51bd83d6d912763ede8416158450acd745bfa1ce
SZEDO-Scripts <szedoscripts01@gmail.com>
2025-04-18 16:01:32 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+
+author 'SZEDO'
+description 'Harvest zones for esx'
+version '1.0.0'
+
+server_scripts {
+    'server.lua',
+}
+
+client_scripts {
+    'client.lua',
+}
+
+shared_script 'config.lua'
+
+dependencies {
+    'es_extended',
+}
